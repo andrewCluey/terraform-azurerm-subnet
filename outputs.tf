a0756c2f2f0677aea2b70364d5482cca9126576f
andrew.clure@irwinmitchell.com <andrew@andrews-air.home>
2021-04-19 14:38:13 +0100

    removed output map

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,3 @@
-
-output "subnet_data" {
-  description = "a map of Subnet Name to Resource ID"
-  value       = zipmap(azurerm_subnet.subnet.name, azurerm_subnet.subnet.id)
-}
-
 output "subnet_id" {
   description = "The Resource Id of the subnet created"
   value       = azurerm_subnet.subnet.id
