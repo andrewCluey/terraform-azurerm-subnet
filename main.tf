22a6bfed7e51ca4145758a2fc71fad4a4ee3f9e1
andrew.clure@irwinmitchell.com <andrew@andrews-air.home>
2021-04-19 12:07:59 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,71 @@
+locals {
+  name_prefix = var.name_prefix != "" ? replace(var.name_prefix, "/[a-z0-9]$/", "$0-") : ""
+  subnet_name = lower("${local.name_prefix}-${var.project_code}-${var.location_short}-${var.environment}")
+  module_tag = {
+    "module" = basename(abspath(path.module))
+  }
+  default_tags = {
+    environment = var.environment
+    project     = var.project_code
+  }
+
+  tags = merge(var.tags, local.module_tag, local.default_tags)
+
+  subnets_outputs           = zipmap(azurerm_subnet.subnet[*].name, azurerm_subnet.subnet[*].id)
+  route_table_rg            = coalesce(var.route_table_rg, var.resource_group_name)
+  network_security_group_rg = coalesce(var.network_security_group_rg, var.resource_group_name)
+}
+
+
+data "azurerm_route_table" "route_table" {
+  count               = var.route_table_name == null ? 0 : 1
+  name                = var.route_table_name
+  resource_group_name = local.route_table_rg
+}
+
+data "azurerm_network_security_group" "nsg_data" {
+  count               = var.network_security_group_name == null ? 0 : 1
+  name                = var.network_security_group_name
+  resource_group_name = local.network_security_group_rg
+}
+
+
+resource "azurerm_subnet" "subnet" {
+  name                 = local.subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.virtual_network_name
+  address_prefixes     = var.subnet_cidr_list
+
+  service_endpoints = var.service_endpoints
+
+  dynamic "delegation" {
+    for_each = var.subnet_delegation
+    content {
+      name = delegation.key
+      dynamic "service_delegation" {
+        for_each = toset(delegation.value)
+        content {
+          name    = service_delegation.value.name
+          actions = service_delegation.value.actions
+        }
+      }
+    }
+  }
+
+  enforce_private_link_endpoint_network_policies = var.enforce_private_link
+}
+
+
+resource "azurerm_subnet_network_security_group_association" "subnet_association" {
+  count = var.network_security_group_name == null ? 0 : 1
+
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = data.azurerm_network_security_group.nsg_data[0].id
+}
+
+resource "azurerm_subnet_route_table_association" "route_table_association" {
+  count = var.route_table_name == null ? 0 : 1
+
+  subnet_id      = azurerm_subnet.subnet.id
+  route_table_id = data.azurerm_route_table.route_table[0].id
+}
---
+++ variables.tf
@@ -0,0 +1,111 @@
+# variables
+variable "name_prefix" {
+  type        = string
+  description = "Optional. The name prefix to use for the vNet. Defaults to 'vn'"
+  default     = "vn"
+}
+
+variable "location" {
+  type        = string
+  description = "The Azure region where the vNet will be created"
+}
+
+variable "location_short" {
+  type        = string
+  description = "An abbreviation to use for the location. Must be less than 4 characters."
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9]{1,4}$", var.location_short))
+    error_message = "Sorry, but the short location abbreviation should be without spaces and less than 4 characters."
+  }
+}
+
+variable "environment" {
+  type        = string
+  description = "The staging environment where the new vNet will be deployed. For example 'Dev'"
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9]{1,5}$", var.environment))
+    error_message = "The environment name should be without spaces and less than 5 characters."
+  }
+}
+
+variable "project_code" {
+  type        = string
+  description = "The code assigned to the project"
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9]{1,8}$", var.project_code))
+    error_message = "The project code should be without spaces and less than 8 characters."
+  }
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the Resource Group where the new vNet will be created."
+  default     = "type"
+}
+
+
+variable "virtual_network_name" {
+  description = "Virtual network name"
+  type        = string
+}
+
+variable "subnet_cidr_list" {
+  description = "The address prefix list to use for the subnet"
+  type        = list(string)
+}
+
+variable "route_table_name" {
+  description = "The Route Table name to associate with the subnet"
+  type        = string
+  default     = null
+}
+
+variable "route_table_rg" {
+  description = "The Route Table RG to associate with the subnet. Default is the same RG as the subnet."
+  type        = string
+  default     = null
+}
+
+variable "network_security_group_name" {
+  description = "The Network Security Group name to associate with the subnets"
+  type        = string
+  default     = null
+}
+
+variable "network_security_group_rg" {
+  description = "The Network Security Group RG to associate with the subnet. Default is the same RG than the subnet."
+  type        = string
+  default     = null
+}
+
+variable "service_endpoints" {
+  description = "The list of Service endpoints to associate with the subnet"
+  type        = list(string)
+  default     = []
+}
+
+variable "enforce_private_link" {
+  description = "Enable or Disable network policies for the private link endpoint on the subnet"
+  type        = bool
+  default     = false
+}
+
+variable "subnet_delegation" {
+  description = <<EOD
+Configuration delegations on subnet
+object({
+  name = object({
+    name = string,
+    actions = list(string)
+  })
+})
+EOD
+  type        = map(list(any))
+  default     = {}
+}
+
+variable "tags" {
+  description = "tags to apply to the new resources"
+  type        = map(string)
+  default     = null
+}
